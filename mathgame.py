993dc78b314b42e4bf5be10bca5ba21aecd548db
zwb <23510462@qq.com>
2016-10-30 21:01:42 +0800

    加入评分功能

--- mathgame.py
+++ mathgame.py
@@ -12,7 +12,7 @@ from random import randint, choice
 ops = {'+':add,'-':sub}
 MAXTRIES = 2
 
-def doprob():
+def doprob(scores):
     op = choice('+-')
     nums = [randint(1,100) for i in range(2)]
     nums.sort(reverse=True)
@@ -24,26 +24,32 @@ def doprob():
             if int(raw_input(pr)) == ans:              
                 break
             if oops == MAXTRIES:
-                print '正确答案是：\n%s%d' % (pr, ans)
+                scores -= 10
+                print '唉，要扣分了。正确答案是：\n%s%d'.decode('utf-8').encode('cp936') % (pr, ans)
+                print scores
             else:
-                print '不对，再想想'
+                print '不对，再想想'.decode('utf-8').encode('cp936')
                 
             oops += 1
         except(KeyboardInterrupt, EOFError, ValueError):
-            print '不对，再想想'
+            print '不对，再想想吧'.decode('utf-8').encode('cp936')
+    return scores
 
 def main():
     times = 0
-    while times<20:
-        doprob()
+    scores = 100
+    while times<10:
+        scores = doprob(scores)
         times+=1
-        print '恭喜你，做对了%d道题' % times
+        print '恭喜你，做对了%d道题'.decode('utf-8').encode('cp936') % times
         try:
-            opt = raw_input('按任意键继续做题，按N退出:[n]').lower()
+            if times == 10:
+                print '恭喜你，你的得分是%d'.decode('utf-8').encode('cp936') % scores
+            opt = raw_input('按任意键继续做题，按N退出:[n]'.decode('utf-8').encode('cp936')).lower()
+			
             if opt and opt[0] == 'n':
                 break
         except (KeyboardInterrupt, EOFError):
-            break
-
+            break	
 if __name__ == '__main__':
     main()
